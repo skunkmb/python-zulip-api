edbfd9c2a2431f4754c4ec7a9b0c4c71b17381ee
Marco Burstein <theskunkmb@gmail.com>
2017-12-24 21:33:39 -0800

    bots: Create a help message and `doc.md` for Reminder Bot.
    The help message and `doc.md` are similar, but bold is used instead of
    Markdown's header elements in the message, because headers don't work
    in Zulip.

---
+++ zulip_bots/zulip_bots/bots/reminder/doc.md
@@ -0,0 +1,60 @@
+# Reminder Bot
+
+## Setup
+
+No setup is required to use Reminder Bot.
+
+To access the help menu, send the command `help`.
+
+## Usage
+
+Reminder Bot can remind you of whatever you want. It has 2 modes: advanced and
+normal.
+
+### Normal
+
+In **normal**, mode you can just send a message to Reminder Bot and have it
+remind you in 5 minutes.
+
+ > @**Reminder Bot** Walk the dog.
+
+…and then 5 minutes later in a private message from Reminder Bot…
+
+ > Walk the dog.
+
+### Advanced
+
+In **advanced** mode you can control when to get the reminder and whether it
+should be public or private. You have to put your reminder in quotes.
+
+For example,
+
+ > @**Reminder Bot** "Water the plants." in 20 minutes
+
+or
+
+ > @**Reminder Bot** "Water the plants." in 20 mins
+
+or even
+
+ > @**Reminder Bot** "Water the plants." in 20 seconds
+
+…and then in a private message…
+
+ > Water the plants.
+
+You can also do
+
+  > @**Reminder Bot** "Take out the trash." public
+
+…and then, in the current stream for all subscribers to see…
+
+ > Take out the trash.
+
+Or, you can do both at once
+
+ > @**Reminder Bot** "Make the bed." in 20 minutes public
+
+…and then, in the current stream 30 minutes later…
+
+ > Make the bed.
--- zulip_bots/zulip_bots/bots/reminder/reminder.py
+++ zulip_bots/zulip_bots/bots/reminder/reminder.py
@@ -12,6 +12,59 @@ REMINDER_ADVANCED_REGEX = re.compile(
     '$'
 )
 
+HELP_RESPONSE = '''
+Reminder Bot can remind you of whatever you want. It has 2 modes: advanced \
+and normal.
+
+---
+
+In **normal**, mode you can just send a message to Reminder Bot and have it \
+remind you in 5 minutes.
+
+ > @**Reminder Bot** Walk the dog.
+
+…and then 5 minutes later in a private message from Reminder Bot…
+
+ > Walk the dog.
+
+---
+
+In **advanced** mode you can control when to get the reminder and whether it \
+should be public or private. You have to put your reminder in quotes.
+
+For example,
+
+ > @**Reminder Bot** "Water the plants." in 20 minutes
+
+or
+
+ > @**Reminder Bot** "Water the plants." in 20 mins
+
+or even
+
+ > @**Reminder Bot** "Water the plants." in 20 seconds
+
+…and then in a private message…
+
+ > Water the plants.
+
+You can also do
+
+  > @**Reminder Bot** "Take out the trash." public
+
+…and then, in the current stream for all subscribers to see…
+
+ > Take out the trash.
+
+Or, you can do both at once
+
+ > @**Reminder Bot** "Make the bed." in 20 minutes public
+
+…and then, in the current stream 30 minutes later…
+
+ > Make the bed.
+'''
+
 class ReminderHandler(object):
     def usage(self) -> str:
         return '''
@@ -25,7 +78,9 @@ class ReminderHandler(object):
 
         reminder_advanced_match = REMINDER_ADVANCED_REGEX.match(content)
 
-        if reminder_advanced_match:
+        if content == 'help' or content == '':
+            bot_handler.send_reply(message, HELP_RESPONSE)
+        elif reminder_advanced_match:
             # The default time is 5 minutes.
             unconverted_time = int(reminder_advanced_match.group('time') or 5)
             is_minutes = (
